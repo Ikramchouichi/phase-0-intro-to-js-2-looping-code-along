526715f0212eb567428e4222e04b2d675bde6a63
Ikram Chouichi <ikram@netronian.com>
2023-11-29 14:34:08 -0500

    Looping Lab

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    debugger;
+function writeCards(names, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+        debugger;
+        thankYouCards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
   }
-
-  return gifts;
+  return thankYouCards; 
 }
 
-wrapGifts(gifts);
-
+const cards = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(cards);
